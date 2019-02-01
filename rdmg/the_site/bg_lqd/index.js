204ae7dab70e0726238b7d95ad9f54885b1433d8
Pavel <rashapasta@gmail.com>
2019-02-01 03:01:58 +0300

    gray light

--- rdmg/the_site/bg_lqd/index.js
+++ rdmg/the_site/bg_lqd/index.js
@@ -87,5 +87,5 @@
         },
         n = i.screen.init("liquid", null, !0),
         a = n.ctx;
-    n.resize(), t = new e(n.width, n.height, 6, "#86F2FF", "#AAFFFB"), o()
+    n.resize(), t = new e(n.width, n.height, 6, "#BFBFBF", "#BFBFBF"), o()
 }();
