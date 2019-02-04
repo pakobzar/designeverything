47f9498330a83c49fca88a6ccb553357ea72950d
Pavel <rashapasta@gmail.com>
2019-02-04 03:54:16 +0300

    1

---
+++ rdmg/the_site/mouse indicator/sketch.js
@@ -0,0 +1,39 @@
+var xvals = [];
+var yvals = [];
+var bvals = [];
+
+function setup()
+{
+  createCanvas(1920, 800);
+  strokeWeight(2);
+}
+
+function draw()
+{
+  background(0,237,34,93);
+
+  for(var i = 1; i < width; i++) {
+    xvals[i-1] = xvals[i];
+    yvals[i-1] = yvals[i];
+    bvals[i-1] = bvals[i];
+  }
+  // Add the new values to the end of the array
+  xvals[width-1] = mouseX;
+  yvals[width-1] = mouseY;
+
+  if(mouseIsPressed) {
+    bvals[width-1] = 0;
+  } else {
+    bvals[width-1] = 255;
+  }
+
+
+  for(var i=1; i<width; i++) {
+    stroke(255);
+    point(i, xvals[i]/3);
+    stroke(0);
+    point(i, height/3+yvals[i]/3);
+    stroke(255);
+    line(i, 2*height/3+bvals[i]/3, i, (2*height/3+bvals[i-1]/3));
+  }
+}
