38f92539cf42c3d955c058d17e384b5abfcb717f
Pavel <rashapasta@gmail.com>
2019-05-14 00:30:17 +0300

    Update index.js

--- rdmg/the_site/bg_lqd/index.js
+++ rdmg/the_site/bg_lqd/index.js
@@ -87,5 +87,5 @@
         },
         n = i.screen.init("liquid", null, !0),
         a = n.ctx;
-    n.resize(), t = new e(n.width, n.height, 6, "#222222", "#323232"), o()
+    n.resize(), t = new e(n.width, n.height, 6, "#181818, "#1F1F1F"), o()
 }();
