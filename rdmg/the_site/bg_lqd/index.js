48b04d43ef064eb6614e4e8eec21c9cbcba04c78
Pavel <rashapasta@gmail.com>
2019-05-13 22:01:37 +0300

    с

--- rdmg/the_site/bg_lqd/index.js
+++ rdmg/the_site/bg_lqd/index.js
@@ -87,5 +87,5 @@
         },
         n = i.screen.init("liquid", null, !0),
         a = n.ctx;
-    n.resize(), t = new e(n.width, n.height, 6, "#5415C9", "#48E8E0"), o()
+    n.resize(), t = new e(n.width, n.height, 6, "#222222", "#323232"), o()
 }();
